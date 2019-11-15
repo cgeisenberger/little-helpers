7c965b6cb6abb5b118f3c93ccc0330d6d7840bfe
Christoph <cgeisenberger@users.noreply.github.com>
2019-11-15 12:16:25 +0100

    Update ud-demux.sh

--- scripts/ud-demux.sh
+++ scripts/ud-demux.sh
@@ -2,7 +2,7 @@
 
 for bc in "$@"
 do
-    echo "grep -A 3 --no-group-separator "@.*:$bc$" Undetermined.R1.fastq > bc.${bc}_R1.fastq"
-    echo "grep -A 3 --no-group-separator "@.*:$bc$" Undetermined.R2.fastq > bc.${bc}_R2.fastq"
+    grep -A 3 --no-group-separator "@.*:$bc$" Undetermined.R1.fastq > bc.${bc}_R1.fastq
+    grep -A 3 --no-group-separator "@.*:$bc$" Undetermined.R2.fastq > bc.${bc}_R2.fastq
 done
   
