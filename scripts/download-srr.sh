d212c57f2c7e20a47b5b5e2d931423e3284be99b
Christoph <cgeisenberger@users.noreply.github.com>
2021-06-24 11:23:20 +0200

    Update download-srr.sh

--- scripts/download-srr.sh
+++ scripts/download-srr.sh
@@ -3,8 +3,17 @@
 # Loops over list of SRR IDs in accession_list.txt and downloads them to /outputdir/
 # NB: Will download to "./" if outputdir is omitted
 
+if [ $# -eq 0 ]
+  then
+    echo "    Usage: download_srr.sh list.txt dir"
+    echo "    Loops over list of SRRs in 'list.txt' and downloads them to 'dir'"
+    echo "    Note: Will save files to current directory if dir is not specified"
+    exit 1
+fi
+
 filename=$1
 output_dir="./"
+
 if [ -n "$2" ]
   then
     output_dir=$2
@@ -18,7 +27,8 @@ for srr in $lines; do
     srr=$(echo $srr | tr -d '\r')
     echo "Starting download for $srr"
     prefix=$(echo $srr | cut -c1-6)
-    add="ftp://ftp-trace.ncbi.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/$prefix/$srr/$srr.sra"
+    #add="ftp://ftp-trace.ncbi.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/$prefix/$srr/$srr.sra"
+    add="https://sra-pub-run-odp.s3.amazonaws.com/sra/$srr/$srr"
     wget -P $output_dir $add
     echo "Done"
 done
